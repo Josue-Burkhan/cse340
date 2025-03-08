686f4e4cc953629d130967bd2904ab210c0cb55a
joshua.burkhan <bai21030@byui.edu>
2025-03-08 16:36:01 -0500

    cse motors website created V0.1

--- routes/static.js
+++ routes/static.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// Servir archivos estáticos desde la carpeta "public"
 router.use(express.static("public"));
 
 module.exports = router;
