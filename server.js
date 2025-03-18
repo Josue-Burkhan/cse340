913c5e0b355fe7dfc24ceacb88d2289bd818ed9b
joshua.burkhan <bai21030@byui.edu>
2025-03-17 20:09:51 -0500

    finish: Learning Activity (1 of 3)

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const cors = require("cors");
+const baseController = require("./controllers/baseController")
+const Util = require("./utilities") 
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * Routes
@@ -18,12 +21,14 @@ app.use(static)
 app.use(cors())
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-
+app.use(Util.addGlobalVariables)
 
 //Index route
 app.get("/", (req, res) => {
-  res.render("layout", { title: "CSE Motors home", body: "index" });
-});
+  res.render("layout", { title: "CSE Motors home", body: "index" })
+})
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
